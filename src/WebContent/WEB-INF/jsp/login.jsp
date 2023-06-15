d6e8090bdb0a6fd3f1d3a58014247c2fa5c5c488
dojo2023-takagawa <130331255+dojo2023-takagawa@users.noreply.github.com>
2023-06-15 17:49:50 +0900

    高川0615

--- src/WebContent/WEB-INF/jsp/login.jsp
+++ src/WebContent/WEB-INF/jsp/login.jsp
@@ -4,9 +4,26 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>ログイン画面</title>
 </head>
 <body>
-
+<!-- <div class="wrapper"> -->
+	<!-- ヘッダー（ここから） -->
+	<h1 id="logo">
+		<img src="img/logo.png" alt="ScheZoole">
+	</h1>
+	<!-- ヘッダー（ここまで） -->
+	<!-- メイン（ここから） -->
+	<h2>ログイン</h2>
+	<div class="main">
+	<form class="form1" method="POST" action="/Esan/LoginServlet">
+		<input type="text" name="username" placeholder="ユーザネーム"><br>
+      	<input type="password" name="password" placeholder="パスワード"><br>
+      	<input type="submit" name="LOGIN" value="ログイン" class="submit"><br>
+      	<a href="/Esan/SignupServlet" id="signup">アカウント作成</a>
+    </form>
+    	<a href="/Esan/InquiryServlet" id="inquiry">お問い合わせ</a>
+    </div>
+    <!-- メイン（ここまで） -->
 </body>
 </html>
--- src/src/servlet/LoginServlet.java
+++ src/src/servlet/LoginServlet.java
@@ -1,11 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.LoginDAO;
 
 /**
  * Servlet implementation class LoginServlet
@@ -18,16 +23,37 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// ログインページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String un = request.getParameter("username");
+		String pw = request.getParameter("password");
 
+		// ログイン処理を行う
+		LoginDAO logDao = new LoginDAO();
+		if (logDao.isLoginOK(new user(username, password))) {	// ログイン成功
+			// セッションスコープにIDを格納する
+			HttpSession session = request.getSession();
+			session.setAttribute("id", new LoginUser(id));
+
+		// メニューサーブレットにリダイレクトする
+		response.sendRedirect("/Esan/Schedule_listServlet");
+		}
+		else {									// ログイン失敗
+			// リクエストスコープに、タイトル、メッセージ、戻り先を格納する
+			request.setAttribute("result", new Result("IDまたはPWに間違いがあります。"));
+
+			// 結果ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
 }
