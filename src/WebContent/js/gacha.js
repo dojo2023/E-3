23431b770cb54c47619c3c78d1bcd0760a8176bb
dojo2023-iwata-kazuki <130334597+dojo2023-iwata-kazuki@users.noreply.github.com>
2023-06-21 17:43:10 +0900

    岩田0621

--- src/WebContent/js/gacha.js
+++ src/WebContent/js/gacha.js
@@ -1,3 +1,10 @@
-/**
- *
- */
+document.querySelector(`.rotateY`).animate(
+	[
+		{ transform: 'rotateY(0deg)' },
+		{ transform: 'rotateY(360deg)' }
+	],
+	{
+		duration: 1000,
+		easing: 'linear',
+		iterations: Infinity
+	});
